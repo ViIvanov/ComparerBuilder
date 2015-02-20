12694496257085b3ea39427afd2e99dc70f223dd
Viacheslav Ivanov <viacheslav.ivanov@gmail.com>
2015-02-20 04:32:08 +0300

    Upd: Test changed.

--- ComparerBuilder.Tests/General.cs
+++ ComparerBuilder.Tests/General.cs
@@ -10,31 +10,32 @@ namespace ComparerBuilder.Tests
 
     [TestMethod]
     public void Run() {
-      var subbuilder = new ComparerBuilder<SubData>()
+      var builderSubData = new ComparerBuilder<SubData>()
         .Add(value => value.Test ?? String.Empty, StringComparer.OrdinalIgnoreCase);
 
-      var basebuilder = new ComparerBuilder<BaseData>()
-        .Add(value => value.Test1)
-        .Add(value => value.Test2);
+      var builderBaseData = new ComparerBuilder<BaseData>()
+        .Add(value => value.Test1 % 2)
+        .Add(value => value.Test2 != null ? value.Test2.Value.Date : default(DateTime));
 
-      var builder = new ComparerBuilder<Data>()
-        .Add(basebuilder)
-        .Add(value => value.SubData1, subbuilder)
-        .Add(value => value.SubData1, subbuilder.BuildEqualityComparer(), subbuilder.BuildComparer());
+      var builderData = new ComparerBuilder<Data>()
+        .Add(builderBaseData)
+        .Add(value => value.SubData1, builderSubData);
 
-      var equalityComparer = builder.BuildEqualityComparerChecked();
+      var equalityComparer = builderData.BuildEqualityComparer();
+      var comparer = builderData.BuildComparer();
 
-      var o = new object();
-      var data1 = new Data(0, o, new SubData("a"));
-      var data2 = new Data(0, o, new SubData("A"));
-      var data2a = new Data(0, o, new SubData("c"));
-      var data3 = new Data(1, null, new SubData("b"));
+      var data1 = new Data(2, DateTime.Now, new SubData("a"));
+      var data2 = new Data(4, DateTime.Now, new SubData("A"));
+      var data3 = new Data(6, DateTime.Now, new SubData("c"));
+      var data4 = new Data(1, null, new SubData("b"));
 
-      var test1 = equalityComparer.Equals(data1, data2);
+      var test1 = equalityComparer.Equals(data1, data2); // True
+      var test2 = equalityComparer.Equals(data2, data3); // False
+      var test3 = comparer.Compare(data1, data4); // -1
 
       try {
-        var test2 = equalityComparer.Equals(data1, data2a);
-      } catch (Exception ex) {
+        var xtest2 = equalityComparer.Equals(data1, data4);
+      } catch (Exception) {
 
         throw;
       }
@@ -42,27 +43,27 @@ namespace ComparerBuilder.Tests
   }
 }
 
-internal class BaseData
+class BaseData
 {
-  public BaseData(int test1 = 0, object test2 = null) {
+  public BaseData(int test1 = 0, DateTime? test2 = null) {
     Test1 = test1;
     Test2 = test2;
   }
 
   public int Test1 { get; }
-  public object Test2 { get; }
+  public DateTime? Test2 { get; }
 }
 
-internal sealed class Data : BaseData
+class Data : BaseData
 {
-  public Data(int test1 = 0, object test2 = null, SubData subData1 = null) : base(test1, test2) {
+  public Data(int test1 = 0, DateTime? test2 = null, SubData subData1 = null) : base(test1, test2) {
     SubData1 = subData1;
   }
 
   public SubData SubData1 { get; }
 }
 
-internal sealed class SubData
+class SubData
 {
   public SubData(string test = null) {
     Test = test;
